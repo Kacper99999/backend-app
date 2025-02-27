6e76ce50b3cdd2a02ffb61df9ba4702d563c25af
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2025-02-27 22:13:18 +0100

    Add put request.

--- server.js
+++ server.js
@@ -22,11 +22,24 @@ app.get('/',(req, res) => {
     res.send('server działą!')
 });
 
+app.get('/api/users',async(req, res) => {
+    try{
+        const users = await User.find();
+        res.status(200).json(users)
+    }
+    catch(err){
+        res.status(400).json({
+            message:'Error fetching users',
+            error:err.message
+        })
+    }
+});
+
 app.post('/api/users', async (req, res) => {
     const {name, email} = req.body;
 
     if(!name || !email){
-        res.status(400).json({message:'Name and email is required'});
+        return res.status(400).json({message:'Name and email is required'});
     }
 
     const newUser = new User ({
@@ -47,7 +60,39 @@ app.post('/api/users', async (req, res) => {
             error:err.message 
         });
     }
-})
+});
+
+app.put('/api/users/:id', async (req, res) => {
+    const { id } = req.params;
+    const { name, email } = req.body;
+
+    if(!name || !email){
+        res.status(400).json({message:"Name and email is requiered"});
+    }
+
+    try{
+        const updatedUser = await User.findByIdAndUpdate(
+            id,
+            { name, email },
+            { new: true }
+        );
+
+        if(!updatedUser){
+            return res.status(404).json({message:'Not found'})
+        }
+        
+        res.status(200).json({
+            message:'User updated sucessfully!',
+        user: updatedUser
+    });
+    }
+    catch(err){
+        res.status(500).json({
+            message:'Error updating user',
+            error:err.message
+        });
+    }
+});
 
 app.listen(PORT,()=> {
     console.log(`server działa na porcie ${PORT}`)
